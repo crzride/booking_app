c6e51ef1f2b90599c0de0084748882eb177b044f
crzride <soloveyzz47@gmail.com>
2026-01-30 13:45:21 +0700

    HotelView(APIView) get, - just for learning purposes

--- booking/booking/urls.py
+++ booking/booking/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+
+from hotels.views import HotelView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/hotels/', HotelView.as_view()),
+    path("api/hotels/<int:pk>/", HotelView.as_view())
 ]
+
+# router = routers.DefaultRouter()
+# router.register(r'api/hotels', HotelView)
--- booking/hotels/views.py
+++ booking/hotels/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.core.serializers import serialize
+from django.shortcuts import get_object_or_404
+from django.template.context_processors import request
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from hotels.models import Hotel
+from hotels.serializers import HotelSerializer
+
+
+class HotelView(APIView):
+
+    def get(self, request):
+        hotels = Hotel.objects.all()
+        serializer = HotelSerializer(hotels, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = HotelSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def delete(self, request, pk):
+        hotel = get_object_or_404(Hotel, pk=pk)
+        hotel.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def patch(self, request, pk):
+        hotel = get_object_or_404(Hotel, pk=pk)
+        serializer = HotelSerializer(hotel, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
